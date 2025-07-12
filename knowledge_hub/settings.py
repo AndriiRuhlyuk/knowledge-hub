af486a1b70edafe34ad3a4623fdb82490510ec12
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-07-12 20:47:49 +0200

    Move SECRET_KEY to .env, update .gitignore and add requirements.txt

--- knowledge_hub/settings.py
+++ knowledge_hub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*-emh(0_d5o$prsb86s02oymwr@mu@_gukm)ip609yo6x1a9_+"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
